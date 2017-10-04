c8827dfbd699d1976ead5398f7e4ec1bf62e9a13
Shoo <root@ishoo.ru>
2017-10-04 09:25:34 +0300

    initital

--- Otchet/Form1.cs
+++ Otchet/Form1.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Otchet
 {
     public partial class Form1 : Form
     {
+        public static string otchetPath
+        {
+            get
+            {
+                return Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "otchet.txt");
+            }
+            
+        }
+
+        public static int GetAllBases()
+        {
+            string[] lines = File.ReadAllLines(otchetPath);
+            int total = 0;
+            foreach (string line in lines)
+            {
+                string[] array = line.Split(',');
+                total += Convert.ToInt32(array[2]);
+            }
+            return total;
+        }
+
+        public static void CreateReport()
+        {
+
+        }
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string today = DateTime.Now.ToString("dd.MM.yyyy");
+            StreamWriter filew = new StreamWriter(otchetPath, append: true);
+            filew.WriteLine(today + "," + textBox1.Text + "," + numericUpDown1.Value +"," + textBox3.Text);
+            filew.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Обновлено " + GetAllBases() + " баз");
+            Process.Start(otchetPath);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
         }
     }
 }
---
+++ Otchet/Form2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Otchet
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string OtchetFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), textBox1.Text + ".txt");
+            if (File.Exists(OtchetFile))
+            {
+                DialogResult overwriteresult = MessageBox.Show("Файл уже существует! Перезаписать?", "Затираем?", MessageBoxButtons.YesNo);
+                if (overwriteresult == DialogResult.Yes)
+                {
+                    try
+                    {
+                        File.Copy(Form1.otchetPath, OtchetFile, overwrite: true);
+                    }
+                    
+                    catch (FileNotFoundException ex)
+                    {
+                        MessageBox.Show(Convert.ToString(ex));
+                        return;
+                    }
+                    DialogResult delfileresult1 = MessageBox.Show("Удаляем данные и начинаем вести отчет заново?", "Sure?", MessageBoxButtons.YesNo);
+                    if (delfileresult1 == DialogResult.Yes)
+                    {
+                        File.Delete(Form1.otchetPath);
+                        return;
+                    }
+                        
+
+                    else
+                    {
+                        MessageBox.Show("Делай с этим что хочешь теперь :)");
+                        return;
+                    }
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Приодите позже :)");
+                    return;
+                    
+                }
+            }
+
+            try
+            {
+                File.Copy(Form1.otchetPath, OtchetFile, overwrite: true);
+            }
+            catch (FileNotFoundException ex2)
+            {
+                MessageBox.Show(Convert.ToString(ex2));
+                return;
+            }
+            DialogResult delfileresult2 = MessageBox.Show("Удаляем данные и начинаем вести отчет заново?", "Sure?", MessageBoxButtons.YesNo);
+            if (delfileresult2 == DialogResult.Yes)
+                File.Delete(Form1.otchetPath);
+            else
+                MessageBox.Show("Делай с этим что хочешь теперь :)");
+                
+        }
+    }
+
+    
+}
